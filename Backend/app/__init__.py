5e485ac83ef9b2895d3a6d1b6e43e5bb9b6dab19
yanchen01 <ychen01@bu.edu>
2019-11-17 00:42:53 -0500

    added web forms

--- Backend/app/__init__.py
+++ Backend/app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from config import Config
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='C://Users/a7089/Desktop/Programming/BostonHacks/realWage/frontend/templates')
 app.config.from_object(Config)
 
 from app import routes
--- Backend/app/routes.py
+++ Backend/app/routes.py
@@ -1,8 +1,21 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 from app import app
+from app.forms import InputForm
 
 @app.route('/')
 @app.route('/index')
 def index():
     data = {'wage': '10000', 'city1': 'New York', 'city2': 'Boston'}     
     return render_template('index.html', data = data)
+
+@app.route('/input', methods = ['GET', 'POST'])
+def input():
+    form = InputForm()
+    if form.validate_on_submit():
+        #flash('Wage requested: {}, City 1: {}, City 2: {}'.format(
+            #form.wage.data, form.city1.data, form.city2.data))
+        return redirect(url_for('result'))
+    return render_template(input.html, form = form)
+
+@app.route('/result'):
+def result():
